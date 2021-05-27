343eadf0c22a9e0f0b9dbb7b750494ba4242cd2d
David Arevalo <davidarevaloc20@gmail.com>
2021-05-27 17:54:39 -0500

    part of the proyect

--- src/javaScript/location.js
+++ src/javaScript/location.js
@@ -1,10 +1,60 @@
+const messageError = document.querySelector("#messageError") 
 const headerLocation = document.querySelector("#headerLocation")
 headerLocation.addEventListener("click", ()=>{
     localion()
 })
 function localion(){
-    navigator.geolocation.getCurrentPosition(function(position) {
-        console.log(position.coords.latitude)
-        console.log(position.coords.longitude)
-    });
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(positionNow, Error);
+      } else {
+        messageError.innerHTML=`
+        <p class="locationError">Geolocation is not supported by this browser.</p>
+        `
+        alert(`Geolocation is not supported by this browser.<i class="fas fa-frown"></i>`)
+      }
+}
+
+async function positionNow(position) {
+    console.log(position.coords.latitude )
+    console.log(position.coords.longitude )
+        try {
+          const res = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=7daaca17f73b8537056bf2d813d49c18`)
+          const data = await res.json()
+          console.log(data)
+          let weatherNivel = document.querySelector("#weatherNivel")
+          weatherNivel.innerHTML=`
+          <p>${Math.floor(data.current.temp - 273.15)}<span>°C</span></p>
+          <p class="weatherNivelCountry">${data.timezone}</p>
+          `
+          //catch
+        } catch (error) {
+          console.log(error)
+        }
+      }
+
+
+
+function Error(error) {
+  switch(error.code) {
+    case error.PERMISSION_DENIED:
+        messageError.innerHTML=`
+      <p class="locationError">User denied the request for Geolocation.</p>
+      `
+      break;
+    case error.POSITION_UNAVAILABLE:
+        messageError.innerHTML=`
+      <p class="locationError">Location information is unavailable.</p>
+      `
+      break;
+    case error.TIMEOUT:
+        messageError.innerHTML=`
+      <p class="locationError">The request to get user location timed out.</p>
+      `
+      break;
+    case error.UNKNOWN_ERROR:
+        messageError.innerHTML=`
+      <p class="locationError">An unknown error occurred.</p>
+      `
+      break;
+  }
 }
